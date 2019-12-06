b8ecc1a63eceafaef74e26f64f298cb4692a2027
liuyunhe <liuyunhe666@aliyun.com>
2019-12-06 11:30:23 +0800

    feat(config.header): create config.header

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -63,6 +63,24 @@ import axios from '../../src/index'
 //   }
 // })
 
+// axios({
+// //   method: 'post',
+// //   url: '/base/post',
+// //   data: {
+// //     a: 1,
+// //     b: 2
+// //   }
+// // })
+// //
+// // const arr = new Int32Array([21, 31])
+// //
+// // axios({
+// //   method: 'post',
+// //   url: '/base/buffer',
+// //   data: arr
+// // })
+
+
 axios({
   method: 'post',
   url: '/base/post',
@@ -72,10 +90,23 @@ axios({
   }
 })
 
-const arr = new Int32Array([21, 31])
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
 
 axios({
   method: 'post',
-  url: '/base/buffer',
-  data: arr
+  url: '/base/post',
+  data: searchParams
 })
--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,35 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildUrl } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
   xhr(config)
 }
 
+// 处理请求config参数
 function processConfig(config: AxiosRequestConfig): void {
+  config.headers = transformHeaders(config)
   config.url = transformURL(config)
   config.data = transformRequestData(config)
 }
 
+// 处理请求url参数
 function transformURL(config: AxiosRequestConfig): string {
   let { url, params } = config
   return buildUrl(url, params)
 }
 
+// 处理请求body数据
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+// 处理请求header数据
+function transformHeaders(config: AxiosRequestConfig): any {
+  let { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
